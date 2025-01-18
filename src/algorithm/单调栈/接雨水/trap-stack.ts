fc774e34137feb3fb85ffab7ac397740b83c544b
JXY001A <jxy001a@aliyun.com>
2025-01-18 11:54:48 +0800

    feat: dev

--- src/algorithm/单调栈/接雨水/trap-stack.ts
+++ src/algorithm/单调栈/接雨水/trap-stack.ts
@@ -5,7 +5,7 @@ export function trap(height: number[]): number {
   const stack:number[] = [0];
 
   for(let i=1;i<height.length;i+=1) {
-    if(height[i]>height[stack[stack.length-1]]) {
+    if(height[i]<height[stack[stack.length-1]]) {
       stack.push(i);
     }else if(height[i] === height[stack[stack.length-1]]) {
       // 已右侧为主，计算宽度
@@ -25,32 +25,3 @@ export function trap(height: number[]): number {
   }
   return sum;
 }
-
-export function trapOfficial(height: number[]): number {
-  const length: number = height.length;
-  const stack: number[] = [];
-  stack.push(0);
-  let resVal: number = 0;
-  for (let i = 1; i < length; i++) {
-      let top = stack[stack.length - 1];
-      if (height[top] > height[i]) {
-          stack.push(i);
-      } else if (height[top] === height[i]) {
-          stack.pop();
-          stack.push(i);
-      } else {
-          while (stack.length > 0 && height[top] < height[i]) {
-              let mid = stack.pop();
-              if (stack.length > 0) {
-                  let left = stack[stack.length - 1];
-                  let h = Math.min(height[left], height[i]) - height[mid];
-                  let w = i - left - 1;
-                  resVal += h * w;
-                  top = stack[stack.length - 1];
-              }
-          }
-          stack.push(i);
-      }
-  }
-  return resVal;
-};
