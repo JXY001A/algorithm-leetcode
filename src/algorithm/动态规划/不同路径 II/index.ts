06884728977efcaf8a5554f43b0f5015ce0986ca
JXY001A <jxy001a@aliyun.com>
2025-01-21 21:24:44 +0800

    feat: dev

--- src/algorithm/动态规划/不同路径 II/index.ts
+++ src/algorithm/动态规划/不同路径 II/index.ts
@@ -11,7 +11,7 @@ export function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
   for(let j=0;j<n && obstacleGrid[0][j] != 1;j+=1) {
     dp[0][j] = 1;
   }
-  
+
 
   for(let i=1;i<m;i+=1) {
     for(let j=1;j<n;j+=1) {
@@ -21,4 +21,33 @@ export function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
   }
 
   return dp[m-1][n-1];
+};
+
+export function uniquePathsWithObstaclesOptimize(obstacleGrid: number[][]): number {
+  const m = obstacleGrid.length;
+  const n =  obstacleGrid[0]?.length;
+
+  const dp = Array(n).fill(0);
+
+  for(let i=0;i<n;i+=1) {
+    if(obstacleGrid[0][i] === 1) {
+      dp[i] = 0;
+    }else if(i===0) {
+      dp[i] = 1;
+    }else {
+      dp[i] = dp[i-1];
+    }
+  }
+  
+
+  for(let i=1;i<m;i+=1) {
+    for(let j=0;j<n;j+=1) {
+      if(obstacleGrid[i][j] === 1) {
+        dp[j] = 0;
+      }else if(j>0){
+        dp[j] += dp[j-1];
+      }
+    }
+  }
+  return dp[n-1];
 };
