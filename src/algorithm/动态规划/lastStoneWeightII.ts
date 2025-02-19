a923c06c2f81c1c5ccc58c10ca1258b85e35a8ac
JXY001A <JXY001A@aliyun.com>
2025-02-19 14:55:25 +0800

    Update lastStoneWeightII.ts

--- src/algorithm/动态规划/lastStoneWeightII.ts
+++ src/algorithm/动态规划/lastStoneWeightII.ts
@@ -1,7 +1,13 @@
-/**
- * @param {number[]} stones
- * @return {number}
- */
-var lastStoneWeightII = function(stones) {
-    
+export function lastStoneWeightII(stones:number[]):number {
+  const sum = stones.reduce((acc,next)=>acc+next,0);
+  const capacity = Math.floor(sum/2);
+
+  const dp = new Array(capacity+1).fill(0);
+  for(let i=0;i<stones.length;i+=1) {
+    for(let j=capacity;j>=stones[i];j-=1) {
+      dp[j] = Math.max(dp[j],dp[j-stones[i]]+stones[i]);
+    }
+  }
+
+  return sum - 2 * dp[capacity];
 };
