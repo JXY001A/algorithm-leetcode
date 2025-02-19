b14bf20f0fa767b3f50e4e1ec340935f85d67e57
JXY001A <JXY001A@aliyun.com>
2025-02-19 14:13:42 +0800

    增加 升级版本 canpartitionPro

--- src/algorithm/动态规划/分割等和子集/index.ts
+++ src/algorithm/动态规划/分割等和子集/index.ts
@@ -19,3 +19,22 @@ export function canPartition(nums: number[]): boolean {
   
   return dp[nums.length][capacity] === capacity;
 };
+
+
+export function canPartitionPro(nums: number[]): boolean {
+  const sum = nums.reduce((acc, cur) => acc + cur, 0);
+  // 两数之和不可能为奇数
+  if(sum % 2 !== 0) return false;
+  
+  const capacity = sum / 2;
+  
+
+  const dp = new Array(capacity+1).fill(0);
+  for(let i=0;i<nums.length;i+=1) {
+    for(let j=capacity;j>=nums[i];j-=1) {
+        dp[i][j] = Math.max(dp[j],dp[j-nums[i-1]] + nums[i-1]);
+    }
+  }
+  
+  return dp[capacity] === capacity;
+};
