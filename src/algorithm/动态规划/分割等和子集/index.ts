d482ef2f0700357eefc532803e21522529daf7b8
JXY001A <JXY001A@aliyun.com>
2025-02-19 14:11:18 +0800

    fix bug

--- src/algorithm/动态规划/分割等和子集/index.ts
+++ src/algorithm/动态规划/分割等和子集/index.ts
@@ -8,9 +8,12 @@ export function canPartition(nums: number[]): boolean {
 
   const dp = new Array(nums.length+1).fill(0).map(()=> new Array(capacity+1).fill(0));
   for(let i=1;i<=nums.length;i+=1) {
-    console.log('i',i)
-    for(let j=capacity;j>=nums[i-1];j-=1) {
-      dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-nums[i-1]] + nums[i-1]);
+    for(let j=capacity;j>=1;j-=1) {
+      if(j>=nums[i-1]) {
+        dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-nums[i-1]] + nums[i-1]);
+      }else {
+        dp[i][j] = dp[i-1][j];
+      }
     }
   }
   
