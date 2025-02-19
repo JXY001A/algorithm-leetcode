b4f0d962067d991653a9a7ab75395d5fe2f4665a
JXY001A <JXY001A@aliyun.com>
2025-02-19 14:16:03 +0800

    canPartitionPro 添加测试用例

--- src/algorithm/动态规划/分割等和子集/index.test.ts
+++ src/algorithm/动态规划/分割等和子集/index.test.ts
@@ -1,4 +1,4 @@
-import { canPartition } from './index';
+import { canPartition,canPartitionPro } from './index';
 describe('canPartition', () => {
     test("等和子集1", () => {
       expect(canPartition([1, 5, 11, 5])).toBe(true);
@@ -11,5 +11,20 @@ describe('canPartition', () => {
 
     test("等和子集2", () => {
       expect(canPartition([1,5,10,6])).toBe(true);
+    });
+});
+
+describe('canPartitionPro', () => {
+    test("等和子集1", () => {
+      expect(canPartitionPro([1, 5, 11, 5])).toBe(true);
+    });
+
+    test("等和子集2", () => {
+      expect(canPartitionPro([1, 2, 3, 5])).toBe(false);
+    });
+
+
+    test("等和子集2", () => {
+      expect(canPartitionPro([1,5,10,6])).toBe(true);
     });
 });
