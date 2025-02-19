86fdcfeb518ed6a6e5166c1b85ae13a339c3ab5e
JXY001A <JXY001A@aliyun.com>
2025-02-19 14:57:56 +0800

    Create lastStoneWeightII.test.ts

---
+++ src/algorithm/动态规划/lastStoneWeightII.test.ts
@@ -0,0 +1,6 @@
+import { lastStoneWeightII } from './lastStoneWeightII';
+describe('最后一块石头重量II', () => {
+    test("lastStoneWeightII 1", () => {
+      expect(lastStoneWeightII([2,7,4,1,8,1])).toBe(1);
+    });
+});
