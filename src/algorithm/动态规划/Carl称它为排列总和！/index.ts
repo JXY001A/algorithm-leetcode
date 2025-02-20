0ca2b1609b9cafd57edd4e15986f311583e01fb6
JXY001A <JXY001A@aliyun.com>
2025-02-20 18:42:56 +0800

    combinationSum4

---
+++ src/algorithm/动态规划/Carl称它为排列总和！/index.ts
@@ -0,0 +1,7 @@
+// 全背包问题
+//TODO: 如果求组合数就是外层for循环遍历物品，内层for遍历背包。
+//TODO: 如果求排列数就是外层for遍历背包，内层for循环遍历物品。
+export function combinationSum4(nums: number[], target: number):number {
+  
+    return 0;
+} 
