52fc7ded726c2ff1350edbf33c19ac152b26d565
JXY001A <JXY001A@aliyun.com>
2025-02-20 16:13:07 +0800

    add test example

--- src/algorithm/动态规划/目标和/findTargetSumWays.test.ts
+++ src/algorithm/动态规划/目标和/findTargetSumWays.test.ts
@@ -1,6 +1,6 @@
 import { findTargetSumWays} from './findTargetSumWays';
 describe('最后一块石头重量II', () => {
     test("findTargetSumWays 1", () => {
-      expect(findTargetSumWays([1, 1, 1, 1, 1],3)).toBe(1);
+      expect(findTargetSumWays([1, 1, 1, 1, 1],3)).toBe(5);
     });
 });
