36be40ddad6d83b8bd6cdd443408bab349b63877
JXY001A <JXY001A@aliyun.com>
2025-02-21 10:49:32 +0800

    add combinationSum4

--- src/algorithm/动态规划/Carl称它为排列总和！/index.ts
+++ src/algorithm/动态规划/Carl称它为排列总和！/index.ts
@@ -2,15 +2,20 @@
 //TODO: 如果求组合数就是外层for循环遍历物品，内层for遍历背包。
 //TODO: 如果求排列数就是外层for遍历背包，内层for循环遍历物品。
 export function combinationSum4(nums: number[], target: number):number {
-    const dp = new Array(target+1).fill(0).map(()=>new Array(nums.length+1).fill(0));
-    dp[0][0] = 1;    
+    const dp = new Array(target+1).fill(0).map(()=>new Array(target+1).fill(0));
+    for(let i=0;i<=target;i+=1) {
+        dp[i][0] = 1;
+    }
 
     for(let i=1;i<=target;i+=1) {
-        for( let j=1;j<=nums.length;j+=1) {
+        for(let t=0;t<i;t+=1) {
+            dp[i][t] = dp[i-1][t]; 
+        }
+        for(let j=1;j<=nums.length;j+=1) {
             if(i>=nums[j-1]) {
-                dp[i][j] = dp[i-1][j] + 
+                dp[i][i] += dp[i][i-nums[j-1]]; 
             }
         }
     }
-    return 0;
+    return dp[target][target];
 } 
