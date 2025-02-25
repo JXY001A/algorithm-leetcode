b7e2f92e545b8204e902a334ffccef80c8ef3b4f
JXY001A <JXY001A@aliyun.com>
2025-02-25 10:49:57 +0800

    add test

--- src/algorithm/动态规划/单词拆分/index.test.ts
+++ src/algorithm/动态规划/单词拆分/index.test.ts
@@ -2,19 +2,19 @@
 import { wordBreak } from "./index";
 
 describe("单词拆分",()=>{
-    test("numSquares 1", () => {
+    test("wordBreak 1", () => {
         const s = "leetcode";
         const wordDict = ["leet", "code"];
         expect(wordBreak(s,wordDict)).toBe(true);
     }); 
 
-    test("numSquares 2", () => {
+    test("wordBreak 2", () => {
         const s = "applepenapple"; 
         const wordDict = ["apple", "pen"];
         expect(wordBreak(s,wordDict)).toBe(true);
     }); 
 
-    test("numSquares 2", () => {
+    test("wordBreak 2", () => {
         const s = "catsandog";
         const  wordDict = ["cats", "dog", "sand", "and", "cat"];
         expect(wordBreak(s,wordDict)).toBe(false);
