1df49f98c2a50bca31de27fbf1eaa33452d56350
JXY001A <jxy001a@aliyun.com>
2025-02-25 09:05:04 +0800

    feat: dev

--- src/algorithm/动态规划/Carl称它为排列总和！/index.ts
+++ src/algorithm/动态规划/Carl称它为排列总和！/index.ts
@@ -1,28 +1,6 @@
 // 全背包问题
 //TODO: 如果求组合数就是外层for循环遍历物品，内层for遍历背包。
 //TODO: 如果求排列数就是外层for遍历背包，内层for循环遍历物品。
-<<<<<<< HEAD
-export const combinationSum4 = (nums, target) => {
-
-  let dp = new Array(target+1).fill(0).map(()=>new Array(target+1).fill(0));
-  for(let i=0;i<=target;i+=1) {
-    dp[i][0] = 1;
-  }
-
-  for(let i = 1; i <= target; i++) {
-    for(let t=0;t<=i;t+=1) {
-      dp[i][t] = dp[i-1][t];
-    }
-    for(let j = 1; j <= nums.length; j++) {
-      if (i >= nums[j-1]) {
-        dp[i][i] +=  dp[i][i-nums[j-1]];
-      }
-    }
-  }
-  console.log('dp', dp);
-  return dp[nums.length][target];
-};
-=======
 export function combinationSum4(nums: number[], target: number):number {
 	const dp = new Array(target+1).fill(0).map(()=>new Array(target+1).fill(0));
 	for(let i=0;i<=target;i+=1) {
@@ -41,4 +19,3 @@ export function combinationSum4(nums: number[], target: number):number {
 	}
 	return dp[target][target];
 } 
->>>>>>> 7071b998c5d31cc1e2e76249d481ec60066b2ed4
---
+++ src/algorithm/动态规划/单词拆分/index.test.ts
@@ -0,0 +1,23 @@
+
+import { wordBreak } from "./index";
+
+describe("单词拆分",()=>{
+    test("numSquares 1", () => {
+        const s = "leetcode";
+        const wordDict = ["leet", "code"];
+        expect(wordBreak(s,wordDict)).toBe(true);
+    }); 
+
+    test("numSquares 2", () => {
+        const s = "applepenapple"; 
+        const wordDict = ["apple", "pen"];
+        expect(wordBreak(s,wordDict)).toBe(true);
+    }); 
+
+    test("numSquares 2", () => {
+        const s = "catsandog";
+        const  wordDict = ["cats", "dog", "sand", "and", "cat"];
+        expect(wordBreak(s,wordDict)).toBe(false);
+    }); 
+});
+
