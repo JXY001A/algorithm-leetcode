82dd6c8e99586ebc06f163c80e6fc92729ac9cde
JXY001A <JXY001A@aliyun.com>
2025-02-25 11:20:41 +0800

    add robII

---
+++ src/algorithm/动态规划/打家劫舍II/index.test.ts
@@ -0,0 +1,16 @@
+import { rob } from "./index";
+
+describe("打家劫舍II",()=>{
+    test("rob 1", () => {
+        expect(rob([2,3,2])).toBe(2);
+    }); 
+
+    test("rob 2", () => {
+        expect(rob([1,2,3,1])).toBe(4);
+    }); 
+
+    test("rob 3", () => {
+        expect(rob([0])).toBe(0);
+    }); 
+});
+
