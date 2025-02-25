90b90a467d08028e26fba14ed90396645e1ab5bb
JXY001A <JXY001A@aliyun.com>
2025-02-25 11:18:06 +0800

    resolve  rob

--- src/algorithm/动态规划/打家劫舍I/index.test.ts
+++ src/algorithm/动态规划/打家劫舍I/index.test.ts
@@ -2,14 +2,10 @@ import { rob } from "./index";
 
 describe("打家劫舍I",()=>{
     test("rob 1", () => {
-        const s = "leetcode";
-        const wordDict = ["leet", "code"];
         expect(rob([1,2,3,1])).toBe(4);
     }); 
 
     test("rob 2", () => {
-        const s = "applepenapple"; 
-        const wordDict = ["apple", "pen"];
         expect(rob([2,7,9,3,1])).toBe(12);
     }); 
 });
--- src/algorithm/动态规划/打家劫舍I/index.ts
+++ src/algorithm/动态规划/打家劫舍I/index.ts
@@ -1,3 +1,17 @@
 export function rob(nums: number[]): number {
-    return 1;
+    // dp[i] ： 表示在第 i 个房间能获得的最大金额
+    // 不能连续只能间隔偷，所以两种情况 偷 i 房间和不偷 i 房间
+    // dp[i-1]： 不偷i房间； dp[i-2]+nums[i]： 偷 i 房间
+    // dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]); 
+    // 不是背包问题直接循环
+    // 初始化 dp[0] = 0;dp[1] = nums[0];
+    // 循环从 i =2 开始；
+    
+    const dp = new Array(nums.length+1).fill(0);
+    dp[0] = 0;
+    dp[1] = nums[0];
+    for(let i=2;i<=nums.length;i+=1) {
+        dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i-1]);
+    }
+    return dp[nums.length];
 };
