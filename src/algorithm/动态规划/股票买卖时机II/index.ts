b0cb3488760ebc56f2fcb5fc0a412dee6a34181e
JXY001A <JXY001A@aliyun.com>
2025-02-26 11:57:03 +0800

    resolve maxProfit three

--- src/algorithm/动态规划/股票买卖时机II/index.ts
+++ src/algorithm/动态规划/股票买卖时机II/index.ts
@@ -1,3 +1,17 @@
 export function maxProfit(prices: number[]): number {
-    return 0;
+    // 多次买卖收益最大，出售手中股票
+    // 有股票时手中现金数，无股票时手中现金数
+    // dp[i][0]: 表示第i天持有股票所得现金 （上一轮买入或 第i天买入 ）
+    // dp[i][1]: 表示第i天不持有股票所得最多现金 (上一轮未买入 或者第 i 天卖出)
+    // dp[i][0] = max(dp[i-1][0],dp[i-1][1]-price[i])
+    // dp[i][1] = max(dp[i-1][1],dp[i-1][0]+price[i])
+    // 初始化：dp[0][0]= -price[0];
+
+    const dp = new Array(prices.length).fill(0).map(()=>new Array(2).fill(0));
+    dp[0][0] = -prices[0]
+    for(let i=1;i<prices.length;i+=1) {
+        dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]-prices[i]);
+        dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0]+prices[i]);
+    }
+    return Math.max(...dp[prices.length-1]);
 };
