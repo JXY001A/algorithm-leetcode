afb5d0623f2098ddc732101664e798f198b712aa
JXY001A <JXY001A@aliyun.com>
2025-02-26 09:03:51 +0800

    resolve  rob three

--- src/algorithm/动态规划/打家劫舍 III/index.ts
+++ src/algorithm/动态规划/打家劫舍 III/index.ts
@@ -27,8 +27,9 @@ function _rob(node?:TreeNode) {
     const leftMaxValsSum =  _rob(node.left);
     const rightMaxValsSum = _rob(node.right);
 
+    // TODO: 如果偷当前节点 node 那么为了利益最大化只能继续投 孙子节点
     const currentValSum = leftMaxValsSum[1] + rightMaxValsSum[1] + node.val;
-    const childValSum = leftMaxValsSum[0] + rightMaxValsSum[0];
-
+    // TODO: 如果不偷当前节点 node 那么子节点 与 孙子节点可以选择最大值的组合来偷，不必拘泥于当前节点不偷就只能偷 子节点而不能偷孙子节点。
+    const childValSum = Math.max(...leftMaxValsSum) + Math.max(...rightMaxValsSum);
     return [currentValSum,childValSum];
 }
