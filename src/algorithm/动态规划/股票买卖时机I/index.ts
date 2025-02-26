11847096c56cfc943a43fc97ff18ef26fbf797da
JXY001A <JXY001A@aliyun.com>
2025-02-26 10:39:26 +0800

    resolve maxProfit

--- src/algorithm/动态规划/股票买卖时机I/index.ts
+++ src/algorithm/动态规划/股票买卖时机I/index.ts
@@ -1,3 +1,16 @@
 export function maxProfit(prices: number[]): number {
-    return 0;
+    // 最大收益的前提：最低买入，最高卖出
+    // 按照时间顺序：最低在前，最高在后
+    // minPrice: 表示 i 天之前最低的价格
+    // maxProfit = Math.max(maxProfit,prices[i]-minPrice)
+    // minPrice = Math.max(minPrice,prices[i]);
+    // 初始化： maxProfit = 0；minPrice=prices[0];
+    // 顺序遍历
+    let minPrice = prices[0];
+    let maxProfit = 0;
+    for(let i=1;i<prices.length;i+=1) {
+        maxProfit = Math.max(maxProfit,prices[i]-minPrice);
+        minPrice = Math.min(minPrice,prices[i]);
+    }
+    return maxProfit;
 };
