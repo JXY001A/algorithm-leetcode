51ee3980fb7e5fd9d906134d75d2e5cff5d1453f
JXY001A <JXY001A@aliyun.com>
2025-02-26 08:42:13 +0800

    add rob three

--- src/algorithm/动态规划/打家劫舍 III/index.ts
+++ src/algorithm/动态规划/打家劫舍 III/index.ts
@@ -11,7 +11,24 @@
  *     }
  * }
  */
-
-function rob(root: TreeNode | null): number {
-    return 1;
+type TreeNode = {
+    val:number;
+    left?:TreeNode;
+    right?:TreeNode;
 };
+
+function rob(root?: TreeNode): number {
+    return Math.max(_rob(root));
+};
+
+function _rob(node?:TreeNode) {
+    if(!node) return [0,0];
+
+    const leftMaxValsSum =  _rob(node.left);
+    const rightMaxValsSum = _rob(node.right);
+
+    const currentValSum = leftMaxValsSum[1] + rightMaxValsSum[1] + node.val;
+    const childValSum = leftMaxValsSum[0] + rightMaxValsSum[0];
+
+    return [currentValSum,childValSum];
+}
