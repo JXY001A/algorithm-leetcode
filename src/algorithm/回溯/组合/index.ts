5095f89786d5e19e7688be7c1fc768ad30ba2400
JXY001A <jxy001a@aliyun.com>
2025-03-03 23:35:23 +0800

    feat: dev

--- src/algorithm/回溯/组合/index.ts
+++ src/algorithm/回溯/组合/index.ts
@@ -1,3 +1,22 @@
 export function combine(n: number, k: number): number[][] {
-    return [];
+    const result: number[][] = [];
+    
+    const _combine = (n:number,start:number,path:number[])=>{
+        if(path.length === k) {
+            result.push([...path]);
+            return;
+        }
+
+        for(let i=start;i<=n;i+=1) {
+            path.push(i);
+            _combine(n,i+1,path);
+            path.pop();
+        }
+    }
+
+    _combine(n,1,[]);
+    
+    return result;
+
 };
+
