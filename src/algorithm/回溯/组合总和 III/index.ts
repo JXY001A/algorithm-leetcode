d591c19d02554e12f8efca4239ff314afbffd1f2
JXY001A <jxy001a@aliyun.com>
2025-03-11 22:39:46 +0800

    feat: dev

--- src/algorithm/回溯/组合总和 III/index.ts
+++ src/algorithm/回溯/组合总和 III/index.ts
@@ -1,16 +1,16 @@
-const sum = (nums:number[])=>{
-    return nums?.reduce((acc,next)=>{
-        return acc + next;
-    },0);
-}
+// const sum = (nums:number[])=>{
+//     return nums?.reduce((acc,next)=>{
+//         return acc + next;
+//     },0);
+// }
 
 export function combinationSum3(k: number, n: number): number[][] {
-    const result:number[][] = [];
+  const result: number[][] = [];
 
-    // 遍历 1 - 9 的数字
-    // 做多使用一次也就是每个组合中数字不允许重复
-    // k 个数字 且 和 等于 n : 
-    /** 
+  // 遍历 1 - 9 的数字
+  // 做多使用一次也就是每个组合中数字不允许重复
+  // k 个数字 且 和 等于 n :
+  /** 
         if(path.length === k) { 
             if(sum(path) === n) {
                 result.push([...path]);
@@ -18,22 +18,27 @@ export function combinationSum3(k: number, n: number): number[][] {
             return ;
         }
     */
-    
-    const _combine = (start:number,end:number,path:number[])=>{
-        if(path.length === k) {
-            if(sum(path) === n) {
-                result.push([...path]);
-            }
-        }
-        for(let i=start;i<=end;i+=1) {
-            path.push(i);
-            _combine(i+1,end,path);
-            path.pop();
-        }
+
+  const _combine = (
+    start: number,
+    end: number,
+    path: number[],
+    sum: number
+  ) => {
+    if (path.length === k) {
+      if (sum === n) {
+        result.push([...path]);
+        return;
+      }
     }
-    
-    _combine(1,9,[]);
+    for (let i = start; i <= 9 - (k - path.length) + 1 && sum < n; i += 1) {
+      path.push(i);
+      _combine(i + 1, end, path, sum + i);
+      path.pop();
+    }
+  };
 
-    return result;
+  _combine(1, 9, [], 0);
 
-};  
+  return result;
+}
