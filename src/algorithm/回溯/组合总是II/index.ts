89c31926577745c5545a94616a3f2b5d53c3e87f
JXY001A <JXY001A@aliyun.com>
2025-03-14 17:32:35 +0800

    add combinationSum 2

--- src/algorithm/回溯/组合总是II/index.ts
+++ src/algorithm/回溯/组合总是II/index.ts
@@ -1,3 +1,20 @@
 export function combinationSum(candidates: number[], target: number): number[][] {
-    return [];
+    const result:number[][] = [];
+    combinationSumRecursive(0,candidates,target,[],result);
+    return result;
 };
+
+const combinationSumRecursive  = (start:number,candidates: number[], target: number,path:number[],result:number[][])=>{
+    if(target<0) return;
+    
+    if(target === 0 && path.length>0) {
+        result.push([...path]);
+        return;
+    }
+
+    for(let i=start;i<candidates.length;i+=1) {
+        path.push(candidates[i]);
+        combinationSumRecursive(i,candidates,target-candidates[i],path,result);
+        path.pop();
+    }
+}
