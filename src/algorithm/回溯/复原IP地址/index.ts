a7df4123a7dfe90d3ae7a1aae6c3d68743a77864
JXY001A <JXY001A@aliyun.com>
2025-03-17 17:22:53 +0800

    resolve restoreIpAddresses

--- src/algorithm/回溯/复原IP地址/index.ts
+++ src/algorithm/回溯/复原IP地址/index.ts
@@ -1,4 +1,25 @@
 export function restoreIpAddresses(s: string): string[] {
     const result:string[] = [];
+
+    restoreIpAddressesRecursive(0,s,[],result);
     return result;
 };
+
+const restoreIpAddressesRecursive = (start:number,s:string,path:string[],result:string[])=>{
+    if(start>=s.length) {
+        if(path.length === 4) {
+            result.push(path.join('.'));
+        }
+        return;
+    }
+
+    for(let i=start;i<start+3 && i<s.length;i+=1) {
+        const ipPart = s.substring(start,i+1);
+        if((ipPart.length>1 && ipPart.startsWith('0')) || Number(ipPart)>255) {
+            continue;
+        }
+        path.push(ipPart);
+        restoreIpAddressesRecursive(i+1,s,path,result);
+        path.pop();
+    }
+}
