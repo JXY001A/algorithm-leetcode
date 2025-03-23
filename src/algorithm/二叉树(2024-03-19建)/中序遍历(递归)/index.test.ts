fdf9c9d8bbe6826dd8fb83f9bd55c9e7f55b0da8
JXY001A <jxy001a@aliyun.com>
2025-03-23 23:25:00 +0800

    feat: test

--- src/algorithm/二叉树(2024-03-19建)/中序遍历(递归)/index.test.ts
+++ src/algorithm/二叉树(2024-03-19建)/中序遍历(递归)/index.test.ts
@@ -1,14 +1,14 @@
-import { preOrderTraversal } from './index';
+import { inOrderTraversal } from './index';
 import { buildBinaryTree } from '../构建二叉树(按层级)/index';
 describe('前序遍历(递归)', () => {
-    test("preOrderTraversal 1", () => {
+    test("inOrderTraversal 1", () => {
         const root:any = buildBinaryTree([1,2,3,4,5,null,8,null,null,6,7,9]);
-        expect(preOrderTraversal(root)).toEqual([1,2,4,5,6,7,3,8,9]);
+        expect(inOrderTraversal(root)).toEqual([4,2,6,5,7,1,3,9,8]);
     });
 
-    test("preOrderTraversal 1", () => {
+    test("inOrderTraversal 1", () => {
         const root:any = buildBinaryTree([1,null,2,3]);
-        expect(preOrderTraversal(root)).toEqual([1,2,3]);
+        expect(inOrderTraversal(root)).toEqual([1,3,2]);
     });
     
 });
