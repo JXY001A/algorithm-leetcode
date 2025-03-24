94b7e91c50be2759c0bfb9eb874e3ded980e7b90
JXY001A <JXY001A@aliyun.com>
2025-03-24 14:48:45 +0800

    feat: resolve isSymmetric

--- src/algorithm/二叉树(2024-03-19建)/对称二叉树/index.test.ts
+++ src/algorithm/二叉树(2024-03-19建)/对称二叉树/index.test.ts
@@ -1,4 +1,4 @@
-import { isSymmetric } from './index';
+import { isSymmetric,isSymmetricTraversal } from './index';
 import { buildBinaryTree } from '../构建二叉树(按层级)/index';
 describe('翻转二叉树', () => {
     test("isSymmetric 1", () => {
@@ -9,6 +9,16 @@ describe('翻转二叉树', () => {
     test("isSymmetric 2", () => {
         const root:any = buildBinaryTree([1,2,2,null,3,null,3]);
         expect(isSymmetric(root)).toEqual(false);
+    });
+
+    test("isSymmetricTraversal 1", () => {
+        const root = buildBinaryTree([1,2,2,3,4,4,3]) as any;
+        expect(isSymmetricTraversal(root)).toEqual(true);
+    });
+
+    test("isSymmetricTraversal 2", () => {
+        const root:any = buildBinaryTree([1,2,2,null,3,null,3]);
+        expect(isSymmetricTraversal(root)).toEqual(false);
     });
     
 });
--- src/algorithm/二叉树(2024-03-19建)/对称二叉树/index.ts
+++ src/algorithm/二叉树(2024-03-19建)/对称二叉树/index.ts
@@ -1,3 +1,49 @@
-export function isSymmetric(root: TreeNode | null): boolean {
-    return root;
+type TreeNode = {
+    val:number;
+    left: TreeNode | null,
+    right: TreeNode | null,
 };
+
+export function isSymmetric(root: TreeNode | null): boolean {
+    return comparerRecursive(root?.left!,root?.right!);
+};
+const comparerRecursive = (left:TreeNode | null,right:TreeNode | null)=>{
+    if(left === null && right !== null) {
+        return false;
+    }
+    if(left !== null && right === null) {
+        return false
+    }
+    if(left===null && right === null) {
+        return true;
+    }
+    if(left?.val !== right?.val) {
+        return false;
+    }
+
+    const outerCompare = comparerRecursive(left?.left!,right?.right!);
+    const innerCompare = comparerRecursive(left?.right!,right?.left!);
+
+    return outerCompare && innerCompare;
+}
+
+export const isSymmetricTraversal = (root:TreeNode|null):boolean=>{
+    if(root === null) return true;
+    const stack:(TreeNode|null)[] = [root.left,root.right];
+    while(stack.length>0) {
+        const tempNode1 = stack.shift();
+        const tempNode2 = stack.shift();
+        if(tempNode1 && tempNode2 ===null) return false;
+        if(tempNode1===null && tempNode2) return false;
+        if(tempNode1 === null && tempNode2 === null) {
+            continue;
+        }
+        if(tempNode1?.val !== tempNode2?.val) return false;
+
+        stack.push(tempNode1?.left!);
+        stack.push(tempNode2?.right!);
+        stack.push(tempNode1?.right!);
+        stack.push(tempNode2?.left!);
+    }
+    return true;
+}
