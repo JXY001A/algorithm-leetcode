c1fc7e0e462dd215ab7aff0d98e10bad73834ea4
JXY001A <JXY001A@aliyun.com>
2025-03-25 13:33:45 +0800

    feat: add binaryTreePaths

--- src/algorithm/二叉树(2024-03-19建)/二叉树的所有路径/index.ts
+++ src/algorithm/二叉树(2024-03-19建)/二叉树的所有路径/index.ts
@@ -1,3 +1,26 @@
-export function binaryTreePaths(root: TreeNode | null): string[] {
-    return []
+type TreeNode = {
+    val:number;
+    left: TreeNode | null,
+    right: TreeNode | null,
 };
+
+export function binaryTreePaths(root: TreeNode | null): string[] {
+    const result:string[] = [];
+    getBinaryTreePaths(root,[],result);
+    return result;
+};
+
+const getBinaryTreePaths = (node: TreeNode | null,path:number[],result:string[])=>{
+    if(!node) return;
+    if(!node?.left && !node?.right) {
+        path.push(node.val);
+        result.push(path.join('->'));
+        path.pop();
+        return;
+    }
+
+    path.push(node.val)
+    getBinaryTreePaths(node.left,path,result);
+    getBinaryTreePaths(node.right,path,result);
+    path.pop();
+}
