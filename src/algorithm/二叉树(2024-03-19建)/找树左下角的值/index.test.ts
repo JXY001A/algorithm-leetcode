1e86e38789447ff8c8071d37630d26b5286f3322
JXY001A <JXY001A@aliyun.com>
2025-03-25 18:03:13 +0800

    resolve  findBottomLeftValue

--- src/algorithm/二叉树(2024-03-19建)/找树左下角的值/index.test.ts
+++ src/algorithm/二叉树(2024-03-19建)/找树左下角的值/index.test.ts
@@ -1,6 +1,6 @@
 import { findBottomLeftValue } from './index';
 import { buildBinaryTree } from '../构建二叉树(按层级)/index';
-describe('左叶子之和', () => {
+describe('找树左下角的值', () => {
     test("findBottomLeftValue 1", () => {
         const root:any = buildBinaryTree([2,1,3]);
         expect(findBottomLeftValue(root)).toEqual(1);
@@ -10,5 +10,10 @@ describe('左叶子之和', () => {
     test("findBottomLeftValue 2", () => {
         const root:any = buildBinaryTree([1,2,3,4,null,5,6,null,null,7]);
         expect(findBottomLeftValue(root)).toEqual(7);
+    });
+    
+    test("findBottomLeftValue 3", () => {
+        const root:any = buildBinaryTree([0]);
+        expect(findBottomLeftValue(root)).toEqual(0);
     });
 });
--- src/algorithm/二叉树(2024-03-19建)/找树左下角的值/index.ts
+++ src/algorithm/二叉树(2024-03-19建)/找树左下角的值/index.ts
@@ -1,8 +1,24 @@
-type Node = {
+type TreeNode = {
     val:number;
-    left: Node | null,
-    right: Node | null,
+    left: TreeNode | null,
+    right: TreeNode | null,
 };
-export function findBottomLeftValue(root: Node | null): number {
-    return 0;
+export function findBottomLeftValue(root: TreeNode | null): number {
+    let maxDepth = 0;
+    let result = 0;
+    const findBottomLeftValueRecursive = (node: TreeNode | null,depth:number)=>{
+        if(!node) return;
+        if(!node.left && !node.right) {
+            if(maxDepth<depth) {
+                result = node.val;
+                maxDepth = depth;
+            }
+        }
+        findBottomLeftValueRecursive(node.left,depth+1);
+        findBottomLeftValueRecursive(node.right,depth+1);
+    }
+
+    findBottomLeftValueRecursive(root,1);
+    return result
 };
+
