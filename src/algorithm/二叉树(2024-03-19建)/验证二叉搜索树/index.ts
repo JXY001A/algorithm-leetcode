0606f304f2611c757aea9366e98b5c58e4cdea25
JXY001A <JXY001A@aliyun.com>
2025-03-26 11:01:31 +0800

    feat: resolve isValidBST

--- src/algorithm/二叉树(2024-03-19建)/验证二叉搜索树/index.ts
+++ src/algorithm/二叉树(2024-03-19建)/验证二叉搜索树/index.ts
@@ -4,5 +4,17 @@ type TreeNode = {
     right: TreeNode | null,
 };
 export function isValidBST(root: TreeNode | null): boolean {
-    return true;
+    let prevVal = Number.MIN_SAFE_INTEGER;
+    const validBST = (node:TreeNode|null)=>{
+        if(!node) return true;
+        const leftIsBST = validBST(node.left);
+        if(prevVal>=node.val) {
+            return false;
+        }else {
+            prevVal = node.val;
+        }
+        const rightIsBST = validBST(node.right);
+        return leftIsBST && rightIsBST;
+    }
+    return validBST(root);
 };
