6be8627fb306f1335bb186f3d7446a31390bc98a
JXY001A <JXY001A@aliyun.com>
2025-03-26 10:26:43 +0800

    feat: resolve mergeTrees

--- src/algorithm/二叉树(2024-03-19建)/合并二叉树/index.ts
+++ src/algorithm/二叉树(2024-03-19建)/合并二叉树/index.ts
@@ -5,5 +5,14 @@ type TreeNode = {
 };
 
 export function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode | null {
-    return null;
+    if(!root1 && !root2) return null;
+    if(root1 && !root2) return root1;
+    if(root2 && !root1) return root2;
+
+    const mergeVal = root1?.val! + root2?.val!;
+    root1!.val = mergeVal;
+    root1!.left = mergeTrees(root1!.left,root2!.left);
+    root1!.right = mergeTrees(root1!.right,root2!.right);
+
+    return root1;
 };
