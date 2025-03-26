8d8d9207b51fe64318d4d4fe144e2a77c09d195a
JXY001A <JXY001A@aliyun.com>
2025-03-26 10:36:27 +0800

    feat: resolve searchBST

--- src/algorithm/二叉树(2024-03-19建)/二叉搜索树中的搜索/index.ts
+++ src/algorithm/二叉树(2024-03-19建)/二叉搜索树中的搜索/index.ts
@@ -6,5 +6,13 @@ type TreeNode = {
 
 
 export function searchBST(root: TreeNode | null, val: number): TreeNode | null {
-    return null;
+    if(!root) return null;
+    
+    if(root.val === val) {
+        return root;
+    }else if(val < root.val) {
+        return searchBST(root.left,val);
+    }else {
+        return searchBST(root.right,val);
+    }
 };
