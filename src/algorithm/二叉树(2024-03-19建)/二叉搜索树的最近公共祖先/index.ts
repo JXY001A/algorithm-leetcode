fd62759b0d6cdc33c302e8f4e5bfa242e5334baa
JXY001A <JXY001A@aliyun.com>
2025-03-26 17:48:13 +0800

    feat: resolve lowestCommonAncestor

--- src/algorithm/二叉树(2024-03-19建)/二叉搜索树的最近公共祖先/index.ts
+++ src/algorithm/二叉树(2024-03-19建)/二叉搜索树的最近公共祖先/index.ts
@@ -5,6 +5,24 @@ type TreeNode = {
 };
 
 
-export function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
-	return null;
+export function lowestCommonAncestor(root: TreeNode | null, p: TreeNode, q: TreeNode): TreeNode | null {
+	if(!root) return null;
+    // if(root.val === p?.val || root.val === q?.val) {
+    //     return root;
+    // }
+    
+    if(root.val > p.val && root.val > q.val) {
+        let leftAncestor = lowestCommonAncestor(root.left,p,q);
+        if(leftAncestor) {
+            return leftAncestor;
+        }
+    }
+    if(root.val<p.val && root.val < q.val) {
+        let rightAncestor = lowestCommonAncestor(root.right,p,q);
+        if(rightAncestor) {
+            return rightAncestor;
+        }
+    }
+
+    return root;
 };
