060a7ff4dbed57224c44e13c2020b04f70ef96b8
JXY001A <JXY001A@aliyun.com>
2025-03-27 15:08:55 +0800

    feat: resolve buildBinaryTree

--- src/algorithm/二叉树(2024-03-19建)/将有序数组转换为二叉搜索树/index.test.ts
+++ src/algorithm/二叉树(2024-03-19建)/将有序数组转换为二叉搜索树/index.test.ts
@@ -2,12 +2,12 @@ import { sortedArrayToBST } from './index';
 import { buildBinaryTree } from '../构建二叉树(按层级)/index';
 describe('二叉搜索树中的插入操作', () => {
     test("sortedArrayToBST 1", () => {
-        const target = buildBinaryTree([0,-3,9,-10,null,5]) as any;
+        const target = buildBinaryTree([0,-10,5,null,-3,null,9]) as any;
         expect(sortedArrayToBST([-10,-3,0,5,9])).toEqual(target);
     });
 
     test("sortedArrayToBST 2", () => {
-        const target = buildBinaryTree([3,1]) as any;
+        const target = buildBinaryTree([1,null,3]) as any;
         expect(sortedArrayToBST([1,3])).toEqual(target);
     });
 });
--- src/algorithm/二叉树(2024-03-19建)/将有序数组转换为二叉搜索树/index.ts
+++ src/algorithm/二叉树(2024-03-19建)/将有序数组转换为二叉搜索树/index.ts
@@ -4,5 +4,11 @@ type TreeNode = {
     right: TreeNode | null,
 };
 export function sortedArrayToBST(nums: number[]): TreeNode | null {
-    return null;
+    if(nums.length === 0) return null;
+    let nodeValIndex =  Math.floor((nums.length-1)/2);
+    return {
+        val:nums[nodeValIndex],
+        left:sortedArrayToBST(nums.slice(0,nodeValIndex)),
+        right:sortedArrayToBST(nums.slice(nodeValIndex+1)),
+    };
 };
