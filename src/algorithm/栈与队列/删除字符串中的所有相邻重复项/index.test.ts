93e127024daef8d77620878ba3a119f46f64b86c
JXY001A <JXY001A@aliyun.com>
2025-03-28 11:24:03 +0800

    feat: resolve removeDuplicates

--- src/algorithm/栈与队列/删除字符串中的所有相邻重复项/index.test.ts
+++ src/algorithm/栈与队列/删除字符串中的所有相邻重复项/index.test.ts
@@ -3,4 +3,8 @@ describe("删除字符串中的所有相邻重复项",()=>{
   test('removeDuplicates 1',()=>{
     expect(removeDuplicates("abbaca")).toBe('ca');
   })
+
+  test('removeDuplicates 2',()=>{
+    expect(removeDuplicates("azxxzy")).toBe('ay');
+  })
 });
--- src/algorithm/栈与队列/删除字符串中的所有相邻重复项/index.ts
+++ src/algorithm/栈与队列/删除字符串中的所有相邻重复项/index.ts
@@ -1,3 +1,18 @@
 export function removeDuplicates(s: string): string {
-    return s;
+    if(!s) return s;
+    const stack:string[] = [];
+    for(let i=0;i<s.length;i+=1) {
+        const curChar = s[i];
+        if(stack.length ===0) {
+            stack.push(curChar);
+        }else {
+            const stackTopChar = stack[stack.length-1];
+            if(stackTopChar === curChar) {
+                stack.pop();
+            }else {
+                stack.push(curChar);
+            }
+        }
+    }
+    return stack.join('');
 };
