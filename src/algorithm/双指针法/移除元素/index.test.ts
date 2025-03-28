7130c832b02e54c3e50afe8d516e04b0eab47d9d
JXY001A <JXY001A@aliyun.com>
2025-03-28 15:31:05 +0800

    feat: resolve removeElement

--- src/algorithm/双指针法/移除元素/index.test.ts
+++ src/algorithm/双指针法/移除元素/index.test.ts
@@ -2,8 +2,19 @@ import {removeElement } from './index';
 describe("移除元素",()=>{
   test('removeElement 1',()=>{
     expect(removeElement([3,2,2,3],3)).toBe(2);
+
   })  
   test('removeElement 2',()=>{
     expect(removeElement([0,1,2,2,3,0,4,2],2)).toBe(5);
   })  
+
+  test('removeElement 1',()=>{
+    expect(removeElement([1],1)).toBe(0);
+  })
+  
+  test('removeElement 1',()=>{
+    expect(removeElement([4,5],4)).toBe(1);
+  })
+
+  
 });
--- src/algorithm/双指针法/移除元素/index.ts
+++ src/algorithm/双指针法/移除元素/index.ts
@@ -1,3 +1,9 @@
 export function removeElement(nums: number[], val: number): number {
-    return 0;
+    let slow = 0;
+    for(let fast=0;fast<nums.length;fast+=1) {
+        if(nums[fast] !== val) {
+            nums[slow++] = nums[fast];
+        }
+    }
+    return slow;
 };
