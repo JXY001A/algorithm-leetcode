20142de74628daeb9bafefbad3293bad7927711b
JXY001A <JXY001A@aliyun.com>
2025-03-28 15:54:41 +0800

    feat: resolve reverseString

--- src/algorithm/双指针法/反转字符串/index.ts
+++ src/algorithm/双指针法/反转字符串/index.ts
@@ -1,3 +1,12 @@
-export function reverseString(s: string[]): void {
-    
+export function reverseString(s: string[]) {
+    let leftIndex = 0;
+    let rightIndex = s.length-1;
+    while(leftIndex < rightIndex) {
+        const temp = s[leftIndex];
+        s[leftIndex] = s[rightIndex];
+        s[rightIndex] = temp;
+        leftIndex+=1;
+        rightIndex-=1;
+    }
+    return s;
 };
