345edeee490307c1f002cb2c7ce3f6c7638cf6ed
JXY001A <JXY001A@aliyun.com>
2025-03-28 11:03:28 +0800

    feat: resolve isValid

--- src/algorithm/栈与队列/有效的括号/index.ts
+++ src/algorithm/栈与队列/有效的括号/index.ts
@@ -1,3 +1,28 @@
-export function isValid(s: string): boolean {
+function isValid(s: string): boolean {
+    if(!s) return false;
+    const stack:string[] = [];
+
+    const leftBucketMap:Record<string,string> = {
+        '(':')',
+        '[':']',
+        '{':'}',
+    };
+
+    for(let i=0;i<s.length;i+=1) {
+        const curBucket = s[i];
+        if(leftBucketMap[curBucket]) {
+            stack.push(curBucket);
+        }else {
+            const leftBucket = stack.pop() || '';
+            if(leftBucketMap?.[leftBucket] !== curBucket) {
+                return false;
+            }
+        }
+    }
+
+    if(stack.length > 0) {
+        return false;
+    }
+
     return true;
 };
