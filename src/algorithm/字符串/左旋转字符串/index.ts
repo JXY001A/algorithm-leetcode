9f0c153c8f0be2c886e1644d469dc1b3c31a0fff
JXY001A <JXY001A@aliyun.com>
2025-03-31 11:14:24 +0800

    feat: resolve reverseLeftWords

--- src/algorithm/字符串/左旋转字符串/index.ts
+++ src/algorithm/字符串/左旋转字符串/index.ts
@@ -1,3 +1,15 @@
+
+const reverse = (strArray:string[],start:number,end:number)=>{
+    while(start<end) {
+        [strArray[start],strArray[end]] = [strArray[end],strArray[start]];
+        start+=1;
+        end-=1;
+    }
+}
 export const reverseLeftWords = (s:string,n:number)=>{
-    return s;
+    const sArrReverse = s.split('').reverse();
+    
+    reverse(sArrReverse,0,sArrReverse.length-n-1);
+    reverse(sArrReverse,sArrReverse.length-n,sArrReverse.length-1);
+    return sArrReverse.join('');
 }
