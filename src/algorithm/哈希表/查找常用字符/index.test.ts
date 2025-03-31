b924c3ae744e52e0ae3d831b8438cdb92afa1997
JXY001A <JXY001A@aliyun.com>
2025-03-31 19:03:44 +0800

    feat: dev

--- src/algorithm/哈希表/查找常用字符/index.test.ts
+++ src/algorithm/哈希表/查找常用字符/index.test.ts
@@ -1,10 +1,10 @@
 import {commonChars} from './index';
 describe("查找常用字符",()=>{
     test('commonChars 1',()=>{
-        expect(commonChars(["bella","label","roller"])).toBe(["e","l","l"]);
+        expect(commonChars(["bella","label","roller"])).toEqual(["e","l","l"]);
     });
 
     test('commonChars 2',()=>{
-        expect(commonChars(["cool","lock","cook"])).toBe(["c","o"]);
+        expect(commonChars(["cool","lock","cook"])).toEqual(["c","o"]);
     });
 });
