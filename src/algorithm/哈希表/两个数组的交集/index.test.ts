9030265c4389980f10dd17ef27b81735ea7da32d
JXY001A <JXY001A@aliyun.com>
2025-04-01 16:58:26 +0800

    feat: resolve intersection

--- src/algorithm/哈希表/两个数组的交集/index.test.ts
+++ src/algorithm/哈希表/两个数组的交集/index.test.ts
@@ -1,10 +1,18 @@
-import {intersection} from './index';
+import {intersection,intersectionSet} from './index';
 describe("两个数组的交集",()=>{
     test('intersection 1',()=>{
-        expect(intersection([1,2,2,1],[2,2])).toBe([2]);
+        expect(intersection([1,2,2,1],[2,2])).toEqual([2]);
     });
 
     test('intersection 2',()=>{
-        expect(intersection([4,9,5],[9,4,9,8,4])).toBe([9,4]);
+        expect(intersection([4,9,5],[9,4,9,8,4])).toEqual([4,9]);
+    });
+
+    test('intersectionSet 1',()=>{
+        expect(intersectionSet([1,2,2,1],[2,2])).toEqual([2]);
+    });
+
+    test('intersectionSet 2',()=>{
+        expect(intersectionSet([4,9,5],[9,4,9,8,4])).toEqual([9,4]);
     });
 });
--- src/algorithm/哈希表/两个数组的交集/index.ts
+++ src/algorithm/哈希表/两个数组的交集/index.ts
@@ -1,3 +1,34 @@
 export function intersection(nums1: number[], nums2: number[]): number[] {
-    return [];
+    const nums1Map = {};
+    for(let i=0;i<nums1.length;i+=1) {
+        if(!nums1Map[nums1[i]]) {
+            nums1Map[nums1[i]]=nums1[i];
+        }
+    }
+
+    const nums2Map = {};
+    for(let j=0;j<nums2.length;j+=1) {
+        if(!nums2Map[nums2[j]]) {
+            nums2Map[nums2[j]]=nums2[j];
+        }
+    }
+
+    const result:number[] = [];
+    Object.keys(nums2Map).forEach((key)=>{
+        typeof nums1Map[key] === "number" &&  result.push(nums1Map[key]);
+    })
+
+
+    return result;
 };
+
+// 高阶可以使用 Set 去重
+
+export const intersectionSet = (nums1: number[], nums2: number[])=>{
+    const nums1Set = new Set(nums1);
+    const resultSet = new Set();
+    for(let i=0;i<nums2.length;i+=1) {
+        nums1Set.has(nums2[i]) && resultSet.add(nums2[i]);
+    }
+    return [...resultSet];
+}
