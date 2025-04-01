ddbd603330ff6cc159eaca88bcffbd8867b7b30d
JXY001A <JXY001A@aliyun.com>
2025-04-01 19:06:52 +0800

    feat: resolve twoSum

--- src/algorithm/哈希表/两数之和/index.ts
+++ src/algorithm/哈希表/两数之和/index.ts
@@ -1,3 +1,11 @@
 export function twoSum(nums: number[], target: number): number[] {
+    const numsTargetMap:Record<string,number> = {};
+    for(let i=0;i<nums.length;i+=1) {
+        if(typeof numsTargetMap[nums[i]] === 'number') {
+            return [numsTargetMap[nums[i]],i];
+        }else {
+            numsTargetMap[target-nums[i]] = i;
+        }
+    }
     return [];
 };
