c9868ddd81b61f6bff718a4dd902036516bee41e
JXY001A <JXY001A@aliyun.com>
2025-04-01 17:33:54 +0800

    feat: resolve isHappy

--- src/algorithm/哈希表/快乐数/index.ts
+++ src/algorithm/哈希表/快乐数/index.ts
@@ -1,3 +1,16 @@
+
 export function isHappy(n: number): boolean {
-    return false;
+    const nRecord:Record<string,boolean> = {};
+    while(n!==1) {
+        if(nRecord[n] || n ===0) return false;
+        let temp = n;
+        let newN = 0;
+        while(temp>0) {
+            newN+= Math.pow((temp%10),2);
+            temp = Math.trunc(temp/10);
+        }
+        nRecord[n] = true; 
+        n = newN;
+    }
+    return true;
 };
