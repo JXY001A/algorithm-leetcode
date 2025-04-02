091e928ea7bfb17aafac26dd91a19982f29e9cde
JXY001A <JXY001A@aliyun.com>
2025-04-02 15:07:40 +0800

    feat: add threeSum

--- src/algorithm/哈希表/三数之和/index.ts
+++ src/algorithm/哈希表/三数之和/index.ts
@@ -1,4 +1,21 @@
 export  function threeSum(nums: number[]): number[][] {
     const result:number[][] = [];
+    const numsSorted = nums.sort((a,b)=>a-b);
+    for(let i=0;i<numsSorted.length;i+=1) {
+        if(i>0 && numsSorted[i] === numsSorted[i-1]) continue;
+        let left = i+1;
+        let right = numsSorted.length-1;
+        while(left < right) {
+            const sum = numsSorted[i] + numsSorted[left] + numsSorted[right];
+            if(sum === 0) {
+                result.push([numsSorted[i] ,numsSorted[left] ,numsSorted[right]]);
+            }
+            while(left < right && numsSorted[left] === numsSorted[left+1]) left+=1;
+            while(left < right && numsSorted[right] === numsSorted[right-1]) right-=1;
+
+            left+=1;
+            right-=1;
+        }
+    }
     return result;
 };
